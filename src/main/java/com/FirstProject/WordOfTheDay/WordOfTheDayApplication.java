755e723c0009747501f8ef0f707348bfeb9f5831
ferdoushi <62441835+FerdoushiNahar@users.noreply.github.com>
2025-09-05 09:06:31 +0200

    Initial Project

---
+++ src/main/java/com/FirstProject/WordOfTheDay/WordOfTheDayApplication.java
@@ -0,0 +1,13 @@
+package com.FirstProject.WordOfTheDay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordOfTheDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordOfTheDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FirstProject/WordOfTheDay/WordOfTheDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FirstProject.WordOfTheDay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordOfTheDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
